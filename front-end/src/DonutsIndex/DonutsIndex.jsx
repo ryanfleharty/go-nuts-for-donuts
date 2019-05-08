5dcaaa8834f07164197f454f91ce5daf809207ad
Ryan Fleharty <ryan.fleharty@gmail.com>
2019-05-08 15:25:27 -0600

    took out the houses

--- front-end/src/DonutsIndex/DonutsIndex.jsx
+++ front-end/src/DonutsIndex/DonutsIndex.jsx
@@ -6,8 +6,7 @@ class DonutsIndex extends Component {
     constructor(){
         super();
         this.state = {
-            donuts: [],
-            houses: []
+            donuts: []
         }
     }
     async getDonuts(){
@@ -18,37 +17,17 @@ class DonutsIndex extends Component {
             donuts: donutsJSON
         })
     }
-    async getHouses(){
-        const houses = await fetch('https://cors-anywhere.herokuapp.com/https://anapioficeandfire.com/api/houses?pageSize=5')
-        const housesJSON = await houses.json();
-        this.setState({
-            houses: housesJSON
-        })
-    }
     componentDidMount(){
         this.getDonuts();
-        this.getHouses();
     }
     render(){
-        console.log("RENDERING THE COMPONENT")
         const donutsList = this.state.donuts.map((donut)=>{
             return <DonutDetail donut={donut}></DonutDetail>
-        })
-        const housesList = this.state.houses.map((house)=>{
-            return <div onClick={(e)=>{
-                console.log("CLICKED AHOUSE");
-                console.log(e.target);
-                e.target.style.display = "none";
-            }}>
-                <h1>{house.name}</h1>
-                <p>{house.coatOfArms}</p>
-            </div>
         })
         return(
             <div>
                 <h1>DONUTS INDEX</h1>
                 { donutsList }
-                { housesList}
             </div>
         )
     }
